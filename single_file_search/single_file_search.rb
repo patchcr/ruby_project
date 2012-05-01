c30ea7187d66f8729f2d2e1c56f3695a973386ce
Greg Sypolt <sypolt@gmail.com>
2012-05-01 15:06:49 -0400

    adding single_file_search ruby script
    searching text file using regex and printing out the results

---
+++ single_file_search/single_file_search.rb
@@ -0,0 +1,19 @@
+# search file text for a pattern and replace it with a given value
+
+def matching
+	# First set the files to search/replace in
+	files = Dir.glob("/Users/gsypolt/github/ruby_project/single_file_search/*")
+
+	# Then set the variables for find/replace
+	@string_regex = /script/
+	@replacement_string = "STRING"
+
+	files.each do |file_name|
+		text = File.read(file_name)
+
+		match_text = text.scan(@string_regex)
+		puts match_text 
+	end
+end
+
+matching
