30e668717a97978b111e9b78c9ed967f1f18489b
Greg Sypolt <sypolt@gmail.com>
2012-05-02 13:36:24 -0400

    minor enhancements

--- text_analyzer/fit_analyzer/fit_scanner.rb
+++ text_analyzer/fit_analyzer/fit_scanner.rb
@@ -2,7 +2,7 @@
 
 def matching
 	# First set the files to search/replace in
-	files = Dir.glob("/Users/gsypolt/github/ruby_project/text_analyzer/*")
+	files = Dir.glob("/Users/gsypolt/github/ruby_project/text_analyzer/fit_analyzer/*.txt*")
 
 	# Then set the variables for find/replace
 	@string_regex = /script/
@@ -11,8 +11,10 @@ def matching
 	files.each do |file_name|
 		text = File.read(file_name)
 
-		match_text = text.scan(@string_regex)
-		puts match_text 
+		# match_text = text.scan(@string_regex)
+		# puts match_text
+		scanner = text.select { |finder| finder =~ /script/ } 
+		puts scanner 
 	end
 end
 
