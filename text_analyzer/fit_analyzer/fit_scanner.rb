5fb8175c74ded827e722cae72de1c9c14c5a9d4d
Greg Sypolt <sypolt@gmail.com>
2012-05-02 19:04:31 -0400

    added print directory and comment module/class for development

--- text_analyzer/fit_analyzer/fit_scanner.rb
+++ text_analyzer/fit_analyzer/fit_scanner.rb
@@ -1,27 +1,24 @@
 # search file text for a pattern and replace it with a given value
 
 #This is just testing out how we should import modules and use the classes
-require_relative "FitScan"
-include FitScan
-temp = FitScan::ComponentDef.new("//div")
+
+# require_relative "FitScan"
+# include FitScan
+# temp = FitScan::ComponentDef.new("//div")
 
 def matching
 
-	# First set the files to search/replace in
 	files = Dir.glob("./*.txt")
 
-
-	# Then set the variables for find/replace
-	@string_regex = /wait/
-	@replacement_string = "STRING"
+	@string_regex = /script/ # instance variables 
 	
-	files.each do |file_name|
-		text = File.read(file_name)
+	files.each do |file_name| 
+		text = File.readlines(file_name) 
 
-		# match_text = text.scan(@string_regex)
-		# puts match_text
-		scanner = text.select { |finder| finder =~ /wait/ } 
-		puts scanner 
+		scanner = text.select { |finder| finder =~ /script/ } 
+		
+		puts Dir.pwd		
+		puts "#{scanner}" 
 	end
 end
 
