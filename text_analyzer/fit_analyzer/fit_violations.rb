ee6c5bd916e9dcaf2f332e547b01345e222894e7
Greg Sypolt <sypolt@gmail.com>
2012-05-04 08:04:53 -0400

    add count for violation
    not sure it is the best method for counting.

--- text_analyzer/fit_analyzer/fit_violations.rb
+++ text_analyzer/fit_analyzer/fit_violations.rb
@@ -10,23 +10,40 @@ def code_violations
 	
 	# file_array = temp_file.each { |line| line.split }
 	
-  i = 0
-  k = ""
-  temp_file.each {|line| 
-    i += 1
-    temp = line.dup # For debugging 
-    j = line =~ /\|script/
+    i = 0
+    count = 0
+    file_array = ""
+  
+    temp_file.each {|line| 
+      i += 1
+      temp = line.dup # For debugging 
+      j = line =~ /\|script/
     
-    if j
-      # insert file_array
-      wait_flag = temp_file.select { |finder| finder =~ /wait/ }
-      puts "First script table found at line: " + i.to_s + " in: " + File.expand_path(type_test)
-      puts "WARNING - # of occurances found" 
-      puts "#{wait_flag}" 
-      return
-    end
+      if j
+        # insert file_array
+        puts "First script table found at line: " + i.to_s + " in: " + File.expand_path(type_test)
+        puts " "
+        # puts temp # for debugging
+         
+          temp_file.each {|find| 
+            temp_c = find.dup
+            c = find =~ /log message/
+            
+            if c 
+              count += 1
+              puts "Count: " + "#{count}" + " found: " "#{temp_c}"
+            end    
+          }
+          
+          
+        # logmsg_flag = temp_file.select { |finder| finder =~ /log message/ }
+        puts "WARNING - " + "#{count}" + " of occurrences found" 
+        # puts "#{logmsg_flag}" 
+             
+        return
+      end
+    }       
   }
- }
  end
 
 code_violations
